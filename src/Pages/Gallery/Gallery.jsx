edd2f29ed44d7b0cd4dd4074415a3bf81b57538f
RifatHossain41 <rifathossain0423@gmail.com>
2023-05-19 09:18:55 +0600

    gallery marquee added

---
+++ src/Pages/Gallery/Gallery.jsx
@@ -0,0 +1,29 @@
+import Marquee from "react-fast-marquee";
+import img2 from '../../assets/image/img-2.jpg'
+import img3 from '../../assets/image/img-3.jpg'
+import img4 from '../../assets/image/img-4.jpg'
+// import img5 from '../../assets/image/img-5.jpg'
+import img6 from '../../assets/image/img-6.jpg'
+import img7 from '../../assets/image/img-7.jpg'
+import img8 from '../../assets/image/img-8.jpg'
+import img9 from '../../assets/image/img-9.jpg'
+
+const Gallery = () => {
+  return (
+    <div>
+      <Marquee>
+        <img className="w-60" src={img2} alt="" />
+        <img className="w-60" src={img3} alt="" />
+        <img className="w-60" src={img4} alt="" />
+        {/* <img className="w-60" src={img5} alt="" /> */}
+        <img className="w-60" src={img6} alt="" />
+        <img className="w-60" src={img7} alt="" />
+        <img className="w-60" src={img8} alt="" />
+        <img className="w-60" src={img9} alt="" />
+   
+      </Marquee>
+    </div>
+  );
+};
+
+export default Gallery;
