77e702200da1303818a3d37d44248f95bb22261d
RifatHossain41 <rifathossain0423@gmail.com>
2023-05-20 23:42:01 +0600

    Added Show Detail Button

---
+++ src/Pages/ToyDetails/ToyDetails.jsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from "react-router-dom";
+
+const ToyDetails = () => {
+  const details = useLoaderData();
+  const { name, sellerName, email, price, rating, quantity, photo, description} = details
+  console.log(details);
+  return (
+    <div>
+      <div className="card mx-auto w-1/2 mt-5 mb-5 glass">
+        <figure>
+          <img src={photo} alt="" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>SellerName: {sellerName}</p>
+          <p>Email: {email}</p>
+          <p>Price: {price}</p>
+          <p>Rating: {rating}</p>
+          <p>Quantity: {quantity}</p>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToyDetails;
